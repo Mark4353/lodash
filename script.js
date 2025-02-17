fd048cfe9f69a7d0ae6d83ec18366e25ec3da436
der1x <130235333+Mark4353@users.noreply.github.com>
2025-02-17 10:04:07 +0200

    fix

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-
 const slider = document.querySelector(".slider__input");
 const image = document.querySelector(".slider__image");
-const field = document.getElementById('game-field');
-const ball = document.getElementById('ball');
+const field = document.getElementById("game-field");
+const ball = document.getElementById("ball");
 // 1
 
-const resizeImage = () =>{
+const resizeImage = () => {
   const scale = slider.value / 100;
   image.style.transform = `scale(${scale})`;
+
 };
 
-slider.addEventListener("input", _.debounce(resizeImage, 100)) ;
-
+slider.addEventListener("input", _.debounce(resizeImage, 100));
 // 2
 
 const moveBall = _.debounce((event) => {
   const fieldRect = field.getBoundingClientRect();
   const clickX = event.clientX - fieldRect.left;
   const clickY = event.clientY - fieldRect.top;
-
   ball.style.top = `${clickY - ball.offsetHeight / 5}px`;
   ball.style.left = `${clickX - ball.offsetWidth / 5}px`;
-}, 200);
+},200);
 
 field.addEventListener("click", moveBall);
